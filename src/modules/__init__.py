ba6051a0f0d09501f5a8c9f1c48da7f4f20649a3
v.yurii.osadychi <v.yurii.osadychi@reddit.com>
2024-11-02 23:38:05 +0000

    hw_8

---
+++ src/modules/__init__.py
@@ -0,0 +1,8 @@
+from modules.birthday import Birthday
+from modules.field import Field
+from modules.phone import Phone
+from modules.name import Name
+from modules.record import Record
+from modules.address_book import AddressBook
+
+__all__ = ["Birthday", "Field", "Phone", "Record", "Name", "AddressBook"]
